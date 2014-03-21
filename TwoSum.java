be47cfb72a3ae7f6f702cf8964fdd95fba3345fd
ketonfly <ketonfly@gmail.com>
2014-03-21 16:30:50 +0800

    Update TwoSum.java
    使用排序的算法，简化运算
    我不知道HashMap的运行机制

--- TwoSum.java
+++ TwoSum.java
@@ -32,3 +32,53 @@ class solution{
         return result;
     }
 }
+
+//modify my program
+public int[] twoSum(int[] numbers, int target) {
+		HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+		int l = numbers.length;
+		int result[] = new int[2];
+        for(int i = 0 ;i<l;i++){
+        	if(hm.get(target - numbers[i])!=null){
+        		if(hm.get(target-numbers[i]).intValue()!=i){
+        			//这里面有问题，这个i必然是第二个数字
+        			result[1] = i+1;
+        			result[0] = hm.get(target-numbers[i]).intValue()+1;
+        			return result;
+        		}
+        	}
+        	else hm.put(numbers[i],i);
+        }
+        return result;
+    }
+
+
+
+// from internet
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int[] result = new int[2];
+    	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	for (int i = 0; i < numbers.length; i++)
+    	{
+    		if (map.containsKey(target - numbers[i]))
+    		{
+    			result[0] = map.get(target-numbers[i]) + 1;
+    			result[1] = i + 1;
+    			break;
+    		}
+    		else
+    		{
+    			map.put(numbers[i], i);
+    		}
+    	}
+        
+        return result;
+    }
+}
+
+
